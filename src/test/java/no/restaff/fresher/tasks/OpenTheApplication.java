cbf149bfe3884c0146dba4f07197515b4c7a8237
admin <hang.nguyen@restaff.no>
2023-05-17 11:47:54 +0700

    [Build]: Project Type become Serenity Maven Project

---
+++ src/test/java/no/restaff/fresher/tasks/OpenTheApplication.java
@@ -0,0 +1,19 @@
+package no.restaff.fresher.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
+import no.restaff.fresher.ui.GoogleSearchPage;
+
+public class OpenTheApplication implements Task {
+
+    GoogleSearchPage googleSearchPage;
+
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(googleSearchPage)
+        );
+    }
+}
