d162bd61aea035df16a848ccabec18162b1d6f41
binzunu1909 <64793517+binzunu1909@users.noreply.github.com>
2023-05-17 22:17:08 +0700

    Update ActivitiesPage.java

--- src/test/java/no/restaff/fresher/ui/ActivitiesPage.java
+++ src/test/java/no/restaff/fresher/ui/ActivitiesPage.java
@@ -6,6 +6,29 @@ import org.openqa.selenium.By;
 public class ActivitiesPage {
     public static Target ADD_NEW_ACTIVITY_BUTTON =
             Target.the("add new activity button")
-                    .locatedBy("");
-
+                    .locatedBy("//span[@class='jss4177']");
+    public static Target ACTIVITY_NAME_FIELD =
+            Target.the("activity name field")
+                    .locatedBy("//label[text()='Activity Name']");
+    public static Target ACTIVITY_CODE_FIELD =
+            Target.the("activity code field")
+                    .locatedBy("//label[text()='Activity Code']");
+    public static Target ACTIVITY_TYPE_SELECTION =
+            Target.the("activity type selection")
+                    .locatedBy("//label[text()='Activity Type']");
+    public static Target ACTIVITY_CATEGORIES_MULTISELECTION =
+            Target.the("activity categories multiselection")
+                    .locatedBy("//label[text()='Activity Categories']");
+    public static Target START_OPTION =
+            Target.the("start option")
+                    .locatedBy("//li[text()='START']");
+    public static Target BERTHING_OPTION =
+            Target.the("berthing option")
+                    .locatedBy("//span[text()='BERTHING']");        
+    public static Target CARGO_OPTION =
+            Target.the("cargo option")
+                    .locatedBy("//span[text()='CARGO']");  
+    public static Target SAVE_BUTTON =
+            Target.the("save button")
+                    .locatedBy("//span[text()='Save']");
 }
